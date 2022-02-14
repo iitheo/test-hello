98e73dec7492bbe5e1e21bc369643f3a02324883
Theodore Kamma <iitheo@yahoo.com>
2022-02-14 17:32:17 +0100

    update

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+	"github.com/gin-gonic/gin"
+)
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
-	router.Run(":5000")
+	router.GET("/", IndexHandler)
+	router.Run()
+}
+
+func IndexHandler(c *gin.Context) {
+	//name := c.Params.ByName("name")
+	//c.JSON(200, gin.H{
+	//	"message": "hello world",
+	//})
+	c.XML(200, Person{FirstName: "Theo", LastName: "K"})
 }
